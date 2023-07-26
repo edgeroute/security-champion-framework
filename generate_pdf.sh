f36528f94d3130101d431a4ca140c439e7fb4780
edgeroute <chris@kerr.ventures>
2023-07-26 08:48:32 -0400

    refresh PDF and script for building PDF

---
+++ generate_pdf.sh
@@ -0,0 +1,2 @@
+!/bin/sh
+pandoc README.md planning/*.md people/*.md marketing/*.md execution/*.md measurement/*.md LICENSE.md -o Security-Champion-Framework.pdf -V geometry:"top=3cm, bottom=3cm, left=2cm, right=2cm"
